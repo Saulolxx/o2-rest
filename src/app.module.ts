64e5378193db1850118cd27848840926c6482280
Saulo Lins Dias <saulo.dias@ntconsult.com.br>
2023-04-27 00:10:13 -0300

    chore: configure typeorm files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import config from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [config] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: +configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.pass'),
+        database: configService.get('database.name'),
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default () => ({
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: +process.env.DATABASE_PORT || 5432,
+    user: process.env.DATABASE_USER || 'pg',
+    pass: process.env.DATABASE_PASS || '',
+    name: process.env.DATABASE_NAME || 'o2database',
+  },
+});
