df6ac959ce893355e3b39ab9ad2a2957998e579b
Saulo Lins Dias <saulo.dias@ntconsult.com.br>
2023-04-27 10:02:02 -0300

    chore: configure migrations up and down

---
+++ src/config/database/datasource.config.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { resolve } from 'path';
+import { DataSource } from 'typeorm';
+import pgDriver from 'pg';
+import dotenv from 'dotenv';
+import configOptions from '../index';
+
+dotenv.config();
+const options = configOptions();
+
+const datasource = new DataSource({
+  driver: pgDriver,
+  type: 'postgres',
+  host: options.database.host,
+  port: +options.database.port,
+  username: options.database.user,
+  password: options.database.pass,
+  database: options.database.name,
+  synchronize: false,
+  migrations: [resolve(__dirname, 'migrations', '*{.ts,.js}')],
+  entities: [resolve(__dirname, '..', '..', '*.entity{.ts,.js}')],
+});
+
+console.log(resolve(__dirname, 'migrations', '*.{ts.js}'));
+
+datasource.initialize();
+
+export default datasource;
