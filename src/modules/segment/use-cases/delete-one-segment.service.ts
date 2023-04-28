cb5f6cb5e2ebc12940db8368f3bd8fd34ebed8cc
Janshen V Brasil Barreto <janshen.barreto@ntconsult.com.br>
2023-04-28 11:13:04 -0300

    refactor: remove search for segment when deleting one

--- src/modules/segment/use-cases/delete-one-segment.service.ts
+++ src/modules/segment/use-cases/delete-one-segment.service.ts
@@ -2,19 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Segment } from '../entity/segment.entity';
-import { GetOneSegment } from './get-one-segment.service';
 
 @Injectable()
 export class DeleteOneSegment {
   constructor(
     @InjectRepository(Segment)
     private segmentRepository: Repository<Segment>,
-    private readonly getOneSegment: GetOneSegment,
   ) {}
 
   public async run(id: number) {
-    await this.getOneSegment.run(id);
-
     return await this.segmentRepository.delete({ id });
   }
 }
