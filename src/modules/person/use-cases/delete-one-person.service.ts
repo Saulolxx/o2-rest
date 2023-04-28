f1687832b1c1240235afde35bd1728fb861a8486
Janshen V Brasil Barreto <janshen.barreto@ntconsult.com.br>
2023-04-28 11:10:55 -0300

    refactor: remove search for person when deleting one

--- src/modules/person/use-cases/delete-one-person.service.ts
+++ src/modules/person/use-cases/delete-one-person.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Person } from '../entity/person.entity';
-import { GetOnePerson } from './get-one-person.service';
 
 @Injectable()
 export class DeleteOnePerson {
   constructor(
     @InjectRepository(Person)
     private personRepository: Repository<Person>,
-    private readonly getOnePerson: GetOnePerson,
   ) {}
 
   public async run(id: number) {
