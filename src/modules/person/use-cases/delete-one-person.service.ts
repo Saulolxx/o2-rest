97cf8bc19719c29ee2ef056242e2765089df1856
Janshen V Brasil Barreto <janshen.barreto@ntconsult.com.br>
2023-04-28 09:35:24 -0300

    feat: add use case of deleting one person

---
+++ src/modules/person/use-cases/delete-one-person.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Person } from '../entity/person.entity';
+import { GetOnePerson } from './get-one-person.service';
+
+@Injectable()
+export class DeleteOnePerson {
+  constructor(
+    @InjectRepository(Person)
+    private personRepository: Repository<Person>,
+    private readonly getOnePerson: GetOnePerson,
+  ) {}
+
+  public async run(id: number) {
+    await this.getOnePerson.run(id);
+
+    return await this.personRepository.delete({ id });
+  }
+}
