55d4f2ee27a60256c7c5a0450f58f760875563a7
Janshen V Brasil Barreto <janshen.barreto@ntconsult.com.br>
2023-05-02 16:23:46 -0300

    fix: error for when a person does not exist

--- src/modules/experience/use-cases/get-all-experience.service.ts
+++ src/modules/experience/use-cases/get-all-experience.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Experience } from '../entities/experience.entity';
 import { Repository } from 'typeorm';
+import { GetOnePerson } from 'src/modules/person/use-cases';
 
 @Injectable()
 export class GetAllExperienceService {
   constructor(
     @InjectRepository(Experience)
     private experiencesRepository: Repository<Experience>,
+    private readonly getOnePerson: GetOnePerson,
   ) {}
 
-  public run(personID: number) {
+  public async run(personID: number) {
+    await this.getOnePerson.run(personID);
+
     return this.experiencesRepository.find({
       where: {
         personID,
