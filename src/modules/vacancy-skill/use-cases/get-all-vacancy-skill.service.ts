80f0bde64c4954f8bdb61253347d195dd4fced90
Janshen V Brasil Barreto <janshen.barreto@ntconsult.com.br>
2023-05-10 17:29:48 -0300

    feat: add message error when cannot find any vacancies skills

--- src/modules/vacancy-skill/use-cases/get-all-vacancy-skill.service.ts
+++ src/modules/vacancy-skill/use-cases/get-all-vacancy-skill.service.ts
@@ -1,20 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { VacancySkill } from '../entities/vacancy-skill.entity';
 import { Repository } from 'typeorm';
+import { GetOneByIdVacancyService } from 'src/modules/vacancy/use-cases';
 
 @Injectable()
 export class GetAllVacancySkillService {
   constructor(
     @InjectRepository(VacancySkill)
     private vacanciesSkillsRepository: Repository<VacancySkill>,
+    private readonly getOneVacancy: GetOneByIdVacancyService,
   ) {}
 
-  public run(vacancyId: number) {
-    return this.vacanciesSkillsRepository.find({
+  public async run(vacancyId: number) {
+    await this.getOneVacancy.run(vacancyId);
+
+    const vacanciesSkills = await this.vacanciesSkillsRepository.find({
       where: {
         vacancyId,
       },
     });
+
+    if (!vacanciesSkills.length)
+      throw new NotFoundException('No vacancies skilss found');
+
+    return vacanciesSkills;
   }
 }
